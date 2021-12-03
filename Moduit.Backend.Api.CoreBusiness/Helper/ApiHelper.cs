343ff78b09bca2d59e6c48fd68c7053345c1e14c
Pambudi Tutuko <pambudi.tutuko@gmail.com>
2021-12-03 09:48:57 +0700

    Add project files.

---
+++ Moduit.Backend.Api.CoreBusiness/Helper/ApiHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moduit.Backend.Api.CoreBusiness.Helper
+{
+    public class ApiHelper
+    {
+        public static readonly string BaseURL = "https://screening.moduit.id/";
+    }
+}
